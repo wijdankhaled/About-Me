187c0e5a37352264e264e56eaf5ef83372232059
wijdankhaled <wijdankhaled178@gmail.com>
2021-04-26 15:02:46 +0300

    about me is done

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+alert("welcome to my page please answer these yes/no Q and Enjoy getting to know me");
+ let Q1 = prompt("1. Do you think that my name has a beautiful meaning ?");
+ Q1.toUpperCase();
+ //console.log('my name is wijdan'+Q1);
+alert ("iam intrest to your openion thx");
+  let Q2= prompt("2. my age is older than 22");
+  Q2.toUpperCase();
+ // console.log(' iam 22'+Q2);
+alert("my age is 22 :)");
+ let Q3 =prompt("3.Do you think I am addicted to coffee?")
+ Q3.toUpperCase();
+// console.log('i love coffie'+Q3);
+alert("omg actuly the coffe is my life");
+ let Q4 = prompt("4.is my cate name lulu?");
+ Q4.toUpperCase();
+ //console.log('cady the name of my cat'+Q4);
+alert("my cat name name is cady she is sooo cute ");
+let Q5 = prompt("am i a lazy person?");
+Q5.toUpperCase();
+//console.log('i love to sleep alot'+Q5);
+alert("iam not alazy person put i love to sleep alot zzzzz");
+
+alert("thanks four your intrest to know me now let me to know your name :) ");
+ let nameU =prompt("what is yor name");
+alert(" nice name "+ nameU);
+alert("i realy thankful " +nameU +" good Buy ");
+
+
+
+
